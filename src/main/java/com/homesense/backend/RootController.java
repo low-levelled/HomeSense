e432c63abafbebac5c1be6dc119603609413408a
Drake Sadosky <duufi@Drakes-MacBook-Air.local>
2025-03-29 20:27:06 -0700

    HTML created, frontend has many rooms

---
+++ src/main/java/com/homesense/backend/RootController.java
@@ -0,0 +1,13 @@
+package main.java.com.homesense.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/index.html";
+    }
+}
